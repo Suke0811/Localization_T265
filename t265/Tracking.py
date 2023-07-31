a32eaf04f1e08a0ca0fb2673c4444bf8f729ade0
Suke0811 <49264928+Suke0811@users.noreply.github.com>
2023-07-30 21:31:45 -0700

    Update: changing the docstring format to Google Add: mkdocs.yml Add: CI for building the page

--- t265/Tracking.py
+++ t265/Tracking.py
@@ -11,40 +11,30 @@ DEFAULT_TIMEOUT = 100
 
 class Tracking:
     def __init__(self, camera_sn=None, connection_retry=DEFAULT_CONNECTION_RETRY, retry_time=DEFAULT_RETRY_TIME,
-                 timeout=DEFAULT_TIMEOUT, ):
+                 timeout=DEFAULT_TIMEOUT):
         """
-        Setting up the camera
-        :param camera_sn: camera serial number. None for the first camera whatever connected
-        :param connection_retry: retry times for connection
-        :param retry_time: retry time for connection
-        :param timeout: retry timeout for camera frame wait. if camera does not respond in timeout, it will return the latest camera pose
+        Setting up the camera.
+
+        Args:
+            camera_sn (str): Camera serial number. None for the first camera that is connected.
+            connection_retry (int): Retry times for connection.
+            retry_time (int): Retry time for connection in milliseconds.
+            timeout (int): Retry timeout for camera frame wait in milliseconds. If the camera does not respond in the given
+                           timeout, it will return the latest camera pose in the default frame.
         """
         self.pose = None
         self.camera_on = False
         self.camera_sn = camera_sn
-        self.retry_time = retry_time / 1000  # convert to seconds from ms
+        self.retry_time = retry_time / 1000  # Convert to seconds from milliseconds
         self.TIMEOUT = timeout
         self.pipe = rs.pipeline()
-        self.config_camera()
+        self._config_camera()
         self.connection_retry = connection_retry
         self.init_T = np.eye(4)
 
-    def config_camera(self):
-        """
-        Configure the camera
-        :return:
-        """
-        self.config = rs.config()
-        if self.camera_sn is None:
-            self.config.enable_stream(rs.stream.pose)
-        else:
-            self.config.enable_device(self.camera_sn)
-
     def start_tracking(self):
         """
-        Start tracking with the camera
-        It will retry for self.connection_retry times
-        :return:
+        Start tracking with the camera. It will retry for self.connection_retry times.
         """
         self.pipe.start(self.config)
         for i in range(self.connection_retry):
@@ -68,10 +58,19 @@ class Tracking:
         else:
             raise ConnectionError("Camera communication errors")
 
+    def _config_camera(self):
+        """
+        Configure the camera.
+        """
+        self.config = rs.config()
+        if self.camera_sn is None:
+            self.config.enable_stream(rs.stream.pose)
+        else:
+            self.config.enable_device(self.camera_sn)
+
     def stop_tracking(self):
         """
-        Stop tracking with the camera
-        :return:
+        Stop tracking with the camera.
         """
         if self.camera_on:
             self.pipe.stop()
@@ -79,9 +78,13 @@ class Tracking:
 
     def update_pose(self, wait=True):
         """
-        Update the pose
-        :param wait: if wait for the next frame
-        :return: None
+        Update the pose.
+
+        Args:
+            wait (bool): If True, waits for the next frame.
+
+        Returns:
+            None
         """
         if not self.camera_on:
             self.start_tracking()
@@ -99,11 +102,17 @@ class Tracking:
     def get_translation(self, frame=None, trans=True, rotation='quat', degrees=False):
         """
         Get the translation of the camera.
-        :param frame: the frame of the translation. if frame is None, return the translation of the default frame.
-        :param trans: if return the translation
-        :param rotation: rotation format. 'quat', or Euler 'xyz', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx', 'ZYX', 'ZXY', 'YXZ', 'YZX', 'XZY', 'XYZ'.
-        :param degrees: units of euler angle
-        :return: np.array([x, y, z, qx, qy, qz, qw]), np.array([x, y, z, rx, ry, rz]) for 'xyz'. follows input euler order in the output
+
+        Args:
+            frame (str): The frame of the translation. If frame is None, return the translation of the default frame.
+            trans (bool): If True, returns the translation.
+            rotation (str): Rotation format. 'quat', or Euler 'xyz', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx', 'ZYX', 'ZXY',
+                            'YXZ', 'YZX', 'XZY', 'XYZ'.
+            degrees (bool): If True, the Euler angles are returned in degrees.
+
+        Returns:
+            np.array: [x, y, z, qx, qy, qz, qw] or [x, y, z, rx, ry, rz] for 'xyz' format. Follows the input Euler
+                      order in the output.
         """
         ret = None
         if frame is None:
@@ -123,10 +132,15 @@ class Tracking:
 
     def _get_translation(self, trans=True, rotation='quat', degrees=False):
         """
-        private function to get the translation of the camera always in the default frame
-        :param rotation: rotation format. 'matrix', 'quat', or Euler 'xyz', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx', 'ZYX', 'ZXY', 'YXZ', 'YZX', 'XZY', 'XYZ'.
-        :param degrees: units of euler angle
-        :return: np.array([x, y, z, qx, qy, qz, qw])
+        Private function to get the translation of the camera always in the default frame.
+
+        Args:
+            rotation (str): Rotation format. 'matrix', 'quat', or Euler 'xyz', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx',
+                            'ZYX', 'ZXY', 'YXZ', 'YZX', 'XZY', 'XYZ'.
+            degrees (bool): If True, the Euler angles are returned in degrees.
+
+        Returns:
+            np.array: [x, y, z, qx, qy, qz, qw]
         """
         if self.pose:
             t = self._vector2np(self.pose.get_pose_data().translation)
@@ -135,13 +149,19 @@ class Tracking:
 
     def to_nparray(self, t, q, trans, rotation, degrees):
         """
-        Convert translation and rotation to np.array
-        :param t: translation np.array([x, y, z])
-        :param q: quaternion np.array([x, y, z, w])
-        :param trans: if return the translation
-        :param rotation: format of rotation. 'quat', or Euler 'xyz', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx', 'ZYX', 'ZXY', 'YXZ', 'YZX', 'XZY', 'XYZ'.
-        :param degrees: units of euler angle
-        :return: flat np.array([x, y, z, qx, qy, qz, qw]), np.array([x, y, z, rx, ry, rz]) for 'xyz'. follows input euler order in the output
+        Convert translation and rotation to np.array.
+
+        Args:
+            t (np.array): Translation np.array([x, y, z]).
+            q (np.array): Quaternion np.array([x, y, z, w]).
+            trans (bool): If True, returns the translation.
+            rotation (str): Format of rotation. 'quat', or Euler 'xyz', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx', 'ZYX', 'ZXY',
+                            'YXZ', 'YZX', 'XZY', 'XYZ'.
+            degrees (bool): If True, the Euler angles are returned in degrees.
+
+        Returns:
+            np.array: Flat np.array([x, y, z, qx, qy, qz, qw]) or np.array([x, y, z, rx, ry, rz]) for 'xyz' format.
+                      Follows the input Euler order in the output.
         """
         ret_list = []
         if trans:
@@ -152,8 +172,10 @@ class Tracking:
 
     def get_velocity(self):
         """
-        Get the velocity of the camera
-        :return: np.array([vx, vy, vz, wx, wy, wz])
+        Get the velocity of the camera.
+
+        Returns:
+            np.array: [vx, vy, vz, wx, wy, wz]
         """
         if self.pose:
             vel = self._vector2np(self.pose.get_pose_data().velocity)
@@ -162,8 +184,10 @@ class Tracking:
 
     def get_acceleration(self):
         """
-        Get the acceleration of the camera
-        :return: np.array([ax, ay, az, awx, awy, awz])
+        Get the acceleration of the camera.
+
+        Returns:
+            np.array: [ax, ay, az, awx, awy, awz]
         """
         if self.pose:
             acc = self._vector2np(self.pose.get_pose_data().acceleration)
@@ -172,9 +196,14 @@ class Tracking:
 
     def get_matrix(self, frame=None):
         """
-        Get the transformation matrix of the camera in a specific frame
-        :param frame: frame of the transformation matrix. if frame is None, return the transformation matrix of the default frame.
-        :return: np.array([r11, r12, r13, tx, r21, r22, r23, ty, r31, r32, r33, tz, 0, 0, 0, 1]).respahe(4,4)
+        Get the transformation matrix of the camera in a specific frame.
+
+        Args:
+            frame (str): Frame of the transformation matrix. If frame is None, return the transformation matrix of
+                         the default frame.
+
+        Returns:
+            np.array: [r11, r12, r13, tx, r21, r22, r23, ty, r31, r32, r33, tz, 0, 0, 0, 1].reshape(4, 4)
         """
         if self.pose:
             trans = self._get_translation()
@@ -199,13 +228,17 @@ class Tracking:
 
     def _quat2other(self, quat, order: str = 'xyzw', format='matrix', degrees=False):
         """
-        Convert quaternion to other format
-        :param quat: quaternion np.array([x, y, z, w]) or np.array([w, x, y, z])
-        :param order: quaternion order, xyzw or wxyz
-        :param format: matrix, quat, or 'xyz, 'xzy', 'yxz', 'yzx', 'zxy', 'zyx', 'ZYX', 'ZXY', 'YXZ', 'YZX', 'XZY', 'XYZ'
-        :param seq: euler sequence. ignored for other formats
-        :param degrees: units of euler angle. ignored for other formats
-        :return: whatever format you want
+        Convert quaternion to other format.
+
+        Args:
+            quat (np.array): Quaternion np.array([x, y, z, w]) or np.array([w, x, y, z]).
+            order (str): Quaternion order, xyzw or wxyz.
+            format (str): Format to convert to. 'matrix', 'quat', or 'xyz', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx', 'ZYX',
+                          'ZXY', 'YXZ', 'YZX', 'XZY', 'XYZ'.
+            degrees (bool): If True, the Euler angles are returned in degrees.
+
+        Returns:
+            np.array: Whatever format you want.
         """
         if order != 'xyzw':
             quat = np.append(quat[1:], quat[0])
@@ -213,30 +246,38 @@ class Tracking:
             return R.from_quat(quat).as_matrix()
         elif format == 'quat':
             return quat
-        elif format == 'xyz' or format == 'xzy' or format == 'yxz' or format == 'yzx' or format == 'zxy' or format == 'zyx' or format == 'ZYX' or format == 'ZXY' or format == 'YXZ' or format == 'YZX' or format == 'XZY' or format == 'XYZ':
+        elif format in ['xyz', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx', 'ZYX', 'ZXY', 'YXZ', 'YZX', 'XZY', 'XYZ']:
             return R.from_quat(quat).as_euler(format, degrees=degrees)
 
     def _vector2np(self, vector):
         """
-        Convert rs.vector to np.array
-        :param vector: pyrealsense2.vector
-        :return: np.array([x, y, z])
+        Convert rs.vector to np.array.
+
+        Args:
+            vector (pyrealsense2.vector): The vector to convert.
+
+        Returns:
+            np.array: [x, y, z]
         """
         return np.array([vector.x, vector.y, vector.z])
 
     def _quat2np(self, quat, order: str = 'xyzw'):
         """
-        Convert rs.quaternion to np.array
-        :param quat: pyrealsense2.quaternion
-        :param order: xyzw or wxyz string
-        :return:
+        Convert rs.quaternion to np.array.
+
+        Args:
+            quat (pyrealsense2.quaternion): The quaternion to convert.
+            order (str): xyzw or wxyz string.
+
+        Returns:
+            np.array: [x, y, z, w]
         """
         if order == 'wxyz':
             return np.array([quat.w, quat.x, quat.y, quat.z])
         else:
             return np.array([quat.x, quat.y, quat.z, quat.w])
 
-    # status functions
+    # Status functions
     def is_camera_on(self):
         return self.camera_on
 
@@ -246,7 +287,6 @@ class Tracking:
 
     def __del__(self):
         """
-        deconstructor turn off the camera upon Python exit
-        :return:
+        Destructor: Turns off the camera upon Python exit.
         """
         self.stop_tracking()
