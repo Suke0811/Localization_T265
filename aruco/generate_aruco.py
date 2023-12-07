5514295e65b439e3ce4ec9d65d9f124bc613ad9e
alvister88 <alvinzhu2022@gmail.com>
2023-12-06 21:44:38 -0800

    Made generate_aruco more modular to take in parameters for generation

--- aruco/generate_aruco.py
+++ aruco/generate_aruco.py
@@ -1,24 +1,69 @@
+from typing import List, Union
 import cv2
 import cv2.aruco as aruco
 import numpy as np
+import os
 
-# Define the ArUco dictionary (Change as needed)
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+# Define the ArUco dictionary constants
+DICT_4X4 = aruco.DICT_4X4_250
+DICT_5X5 = aruco.DICT_5X5_250
+DICT_6X6 = aruco.DICT_6X6_250
+DICT_7X7 = aruco.DICT_7X7_250
 
-# Specify the IDs of ArUco tags to generate
-ids_to_generate = [0, 1, 2, 3, 4, 5]
+# Mapping of dictionary constants to their string names
+DICT_MAPPING = {
+    DICT_4X4: "DICT_4X4",
+    DICT_5X5: "DICT_5X5",
+    DICT_6X6: "DICT_6X6",
+    DICT_7X7: "DICT_7X7"
+}
 
-# Size of the ArUco marker images (in pixels)
-marker_size = 200
 
-for id in ids_to_generate:
-    # Generate the marker
-    img = aruco.drawMarker(aruco_dict, id, marker_size)
+"""
+Generate ArUco markers for the given IDs and tag types.
 
-    # Save the marker to a file
-    cv2.imwrite(f"aruco_marker_{id}.png", img)
+:param ids: List of IDs for the ArUco markers.
+:param tag_types: ArUco tag type or a list of tag types to be used.
+:param size: Size of the ArUco markers.
+:param file_format: File format for the output images.
+:param directory: Directory where the ArUco markers will be saved.
+"""
+def aruco_generator(ids: List[int], 
+                    tag_types: Union[int, List[int]] = DICT_6X6, 
+                    size: int = 200, 
+                    file_format: str = 'png', 
+                    directory: str = 'aruco_markers') -> None:
+    
+    if isinstance(tag_types, int):
+        tag_types = [tag_types]
 
-    # Optionally display the generated marker
-    cv2.imshow(f"Marker ID: {id}", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    # Loop through each tag type
+    for tag_type in tag_types:
+        # Get the ArUco dictionary
+        aruco_dict = aruco.Dictionary_get(tag_type)
+        tag_type_str = DICT_MAPPING[tag_type] # Get the string representation
+        
+        # Generate markers for each id
+        for id in ids:
+            # Generate the marker
+            img = aruco.drawMarker(aruco_dict, id, size)
+            
+            # Construct the file name
+            file_name = f"{tag_type_str}_id{id}.{file_format}"
+            file_path = os.path.join(directory, file_name)
+            
+            # Save the marker to a file
+            cv2.imwrite(file_path, img)
+            
+    print(f"Generated {len(ids) * len(tag_types)} ArUco markers in '{directory}' directory.")
+
+
+# Example usage:
+ids = [6]
+tag_types = [DICT_6X6] 
+# Generate ArUco markers
+aruco_generator(ids, tag_types, 200, "png", "DICT_6X6_250")
