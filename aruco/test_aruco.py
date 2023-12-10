2a22e3996545369c954c9bc962351720029fce4b
alvister88 <alvinzhu2022@gmail.com>
2023-12-09 23:33:07 -0800

    modified test_aruco into a test function

--- aruco/test_aruco.py
+++ aruco/test_aruco.py
@@ -2,35 +2,44 @@
 from ArucoDetector import ArucoDetector
 import cv2
 
-# Initialize the ArucoDetector
-aruco_detector = ArucoDetector(cv2.aruco.DICT_5X5_250)
 
-# Start the camera stream
-aruco_detector.start_stream()
+def aruco_test():
+    # Initialize the ArucoDetector
+    aruco_detector = ArucoDetector(cv2.aruco.DICT_5X5_250)
 
-# Set the exposure of the camera
-# aruco_detector.auto_calibration()  # Make sure this value is within the supported range
+    # Start the camera stream   
+    aruco_detector.start_stream()
 
-while True:
-    # Process frames from the camera
-    aligned_depth_frame, color_image = aruco_detector.process_frames()
-    if color_image is not None and aligned_depth_frame is not None:
-        # Update marker positions in the frame
-        color_image = aruco_detector.update_marker_positions(color_image, aligned_depth_frame)
-        # Display the image
-        cv2.imshow('RealSense Color', color_image)
+    # Set the exposure of the camera
+    # aruco_detector.auto_calibration()  # Make sure this value is within the supported range
 
-    key = cv2.waitKey(1) & 0xFF
+    while True:
+        # Process frames from the camera
+        aligned_depth_frame, color_image = aruco_detector.process_frames()
+        if color_image is not None and aligned_depth_frame is not None:
+            # Update marker positions in the frame
+            color_image = aruco_detector.update_marker_positions(color_image, aligned_depth_frame)
+            # Display the image
+            cv2.imshow('RealSense Color', color_image)
 
-    # Check for 'c' key to run auto_calibration
-    if key == ord('c'):
-        aruco_detector.auto_calibration()
+        key = cv2.waitKey(1) & 0xFF
 
-    # Check for 'r' key to reset exposure
-    elif key == ord('r'):
-        aruco_detector.reset_exposure()
+        # Check for 'c' key to run auto_calibration
+        if key == ord('c'):
+            aruco_detector.auto_calibration()
 
-    # Break the loop when 'q' is pressed
-    elif key == ord('q'):
-        break
+        # Check for 'r' key to reset exposure
+        elif key == ord('r'):
+            aruco_detector.reset_exposure()
 
+        # Break the loop when 'q' is pressed
+        elif key == ord('q'):
+            break
+
+    # Clean up resources properly
+    aruco_detector.uninitialize()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    aruco_test()
